c18ef2a5a0832187e0f3f113d63c9a6a29ec157e
James McCreanor <james.mccreanor@solnet.co.nz>
2014-10-24 11:29:08 +1300

    Modified main to create server/clients

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,22 @@
 package main;
 
+import server.Server;
+import client.ClientConnection;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		for (int i = 0; i < args.length; i++) {
+			System.out.println("args[" + i + "]: " + args[i]);
+		}
+		
+		if (args[0].equals("-server")) {
+		    Server s = new Server(1234, 3);
+		    
+		}
+		else if (args[0].equals("-client")) {
+			ClientConnection c = new ClientConnection("localhost", 1234);
+		}
 	}
 }

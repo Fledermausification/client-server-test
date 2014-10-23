099bc9a3399fcba4759da80a42d30ed76a35d79e
James McCreanor <james.mccreanor@solnet.co.nz>
2014-10-24 10:30:06 +1300

    Create and display a deck in Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,19 @@
 package main;
 
+import game.Card;
+import game.Deck;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		Deck d = new Deck();
+		System.out.println(d.size() + " cards in the deck!");
+		
+		int i = 1;
+		while (d.size() > 0) {
+			Card c = d.draw();
+			System.out.println(i++ + ": " + c);
+		}
 	}
 }

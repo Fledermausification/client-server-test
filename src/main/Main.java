5e2bde8344b31a4456f3487ab28cd42f1735fd78
James McCreanor <james.mccreanor@solnet.co.nz>
2014-10-24 15:11:01 +1300

    Created basic chat GUI

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,11 @@
 package main;
 
+import gui.CardFrame;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		CardFrame cf = new CardFrame();
 	}
 }

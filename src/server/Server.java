017b0b5d8f7229686481fad85964dd099cae5ea8
Fledermausification <jmccrean@Gmail.com>
2014-10-30 16:58:26 +1300

    Server allows clients to communicate with eachother.

--- src/server/Server.java
+++ src/server/Server.java
@@ -6,44 +6,36 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
-	private Socket[] client;
-	private PrintWriter[] out;
-	private BufferedReader[] in;
+	private List <ClientThread> clients;
 	
 	public Server(int port, int maxClients) {
 		try {
 			ServerSocket ss = new ServerSocket(port);
 			System.out.println("ServerSocket created. inet: " + ss.getInetAddress() + ", local: " + ss.getLocalSocketAddress() + ", port: " + ss.getLocalPort());
 			
+			clients = new ArrayList <ClientThread> ();
+			
 			int i = 0;
-			client = new Socket[maxClients];
-			out    = new PrintWriter[maxClients];
-			in     = new BufferedReader[maxClients];
 			
 			while (i < maxClients) {
-			    client[i] = ss.accept();
-	            out[i] = new PrintWriter(client[i].getOutputStream(), true);
-	            in[i]  = new BufferedReader(new InputStreamReader(client[i].getInputStream()));
+			    Socket s = ss.accept();
+	            
+	            ClientThread ct = new ClientThread(s);
+	            ct.start();
+	            clients.add(ct);
 	            
 				System.out.println("Client connected " + i++);
 			}
 			
 			System.out.println("All clients connected!");
 			
-			for (PrintWriter p : out) {
+			/*for (PrintWriter p : out) {
 				p.println("SERVER MESSAGE - Let's have a clean figh- I mean game!");
-			}
-			
-			while (true) {
-				try {
-					Thread.sleep(100);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
+			}*/
 			
 			
             /*String inputLine, outputLine;
@@ -56,6 +48,58 @@ public class Server {
 		} catch (IOException e) {
 			System.out.println("Exception caught when trying to listen on port " + port + " or listening for a connection");
 	        System.out.println(e.getMessage());
+		}
+		
+		while (true) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public synchronized void broadcast(String message) {
+		for (ClientThread ct : clients) {
+			ct.writeMessage(message);
+		}
+	}
+	
+	private class ClientThread extends Thread {
+		private Socket         socket;
+		private PrintWriter    out;
+		private BufferedReader in;
+		
+		public ClientThread(Socket s) {
+			socket = s;
+			
+			//Create the input/output stream reader/writers
+            try {
+				in  = new BufferedReader(new InputStreamReader(s.getInputStream()));
+	            out = new PrintWriter(s.getOutputStream(), true);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		public void writeMessage(String message) {
+			out.println(message);
+		}
+		
+		public void run() {
+			while (true) {
+				//Wait for a message
+				String message;
+				try {
+					message = in.readLine();
+					broadcast(message);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 }
--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -1,5 +1,28 @@
 package server;
 
-public class ServerThread {
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.util.List;
 
+public class ServerThread extends Thread {
+	private List <?> others;
+	private BufferedReader in;
+	private PrintWriter    out;
+	
+	public ServerThread(BufferedReader in, PrintWriter out) {
+		this.in  = in;
+		this.out = out;
+	}
+	
+	public void run() {
+		while (true) {
+			
+			try {
+				sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
